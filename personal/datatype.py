a162ecf13ee6e607d00a6b7f47a43572744678f7
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-04-01 15:13:32 +0530

    Python Codes

---
+++ personal/datatype.py
@@ -0,0 +1,79 @@
+# name1 = "sky"
+# name2 = 'blue'
+# name3 = """hello world"""
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+# age = 20
+# old = False
+# a = None
+# print(type(old))
+# print(type(a))
+
+#arithmetic operators
+# a = 5
+# b = 2
+# sum = a+b
+# print(sum)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)   # remainder
+# print(a**b)  # exponentiation
+# print(a//b)  # floor division 
+
+
+# #relational operators
+# a =50
+# b =20
+# print(a == b) #equal to
+# print(a != b) #not equal to
+# print(a>b)  #greater than
+# print(a<b)  #less than
+# print(a>=b) #greater than or equal to
+# print(a<=b) #less than or equal to
+
+#assignment operators
+# num =10
+# num +=5
+# print(num)
+# num -=4
+# print("num:", num)
+# num *= 5
+# print("num:", num)
+# num /= 5
+# print("num:", num)
+# num %= 3
+# print("num:", num)
+# num **= 2
+# print("num:", num)
+
+
+#logical operators
+print(not True)
+print(not False)
+a = 40
+b = 20
+print(not (a>b)) #False
+
+val1 = True
+val2 = True
+print("AND operator:",val1 and val2) 
+
+val1 = True
+val2 = False
+print("AND operator:",val1 and val2) 
+
+val1 = True
+val2 = False
+print("OR operator:",val1 or val2)
+
+
+val1 = False
+val2 = False
+print("OR operator:",val1 or val2) 
+
+print("OR operator:" , (a == b) or (a > b)) #True
