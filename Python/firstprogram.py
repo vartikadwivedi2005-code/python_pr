c69b57be6d67346b93cfabc330f3f52350c25615
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-04-04 21:46:36 +0530

    Python Tutorials

--- Python/firstprogram.py
+++
@@ -1,8 +0,0 @@
-# This is my first program in Python. It will print some basic information about me and perform a simple arithmetic operation.
-# print is a built-in function in Python that is used to display output on the console. It can take multiple arguments and will print them separated by a space by default. You can also specify a different separator using the 'sep' parameter.
-print("Hello World")
-print("Vartika is my name.")
-print("My age is 20.")
-print("I am curently pursuing B.Tech in Computer Science and Engineering.")
-print(35)
-print(35+45)
